625a6faeee4c813ae2486f6898f7d800a13050c3
anna <annab35515@gmail.com>
2022-12-12 16:00:30 +0300

    fix:fixed code

---
+++ js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function news () {
+    let click = document.querySelector(".nav_item3");
+    click.onclick = function () {window.location.href = "https://annabelkkk.github.io/web/html/page3/index.html";};
+}
+
+function  home() {
+    let click = document.querySelector(".nav_item3");
+    click.onclick = function () {window.location.href = "https://annabelkkk.github.io/web/html/page1/index.html";};
+}
