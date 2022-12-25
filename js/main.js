f716dad68548d1a7c25196c6a4998844e353dac4
anna <annab35515@gmail.com>
2022-12-25 21:16:51 +0300

    fix:fixed code

--- js/main.js
+++ js/main.js
@@ -14,3 +14,8 @@ function info() {
     let click = document.querySelector(".nav_item");
     click.onclick = function() { window.location.href = "https://annabelkkk.github.io/web/html/page2/index.html"; };
 }
+
+function homelogo() {
+    let click = document.querySelector(".header__logo");
+    click.onclick = function() { window.location.href = "https://annabelkkk.github.io/web/html/page1/index.html"; };
+}
